075a63f184e3a736095ebcd785e03dfbdf52ff11
philipp.proff <philipp.proff@gmx.de>
2023-01-14 16:26:51 +0100

    Remove Text Classifiers

--- Text_Classifier/extractor.py
+++
@@ -1,9 +0,0 @@
-#following tutorial for extracting tokens in text https://www.nltk.org/book/ch03.html
-from pdfminer.high_level import extract_text #package for extracting raw text data from the PDFs
-import nltk
-text = extract_text('/Users/proff/Documents/Paper/tiqqun-the-cybernetic-hypothesis.pdf')#example PDF of Tiqqun
-tokens = nltk.word_tokenize(text)
-analyzed_text = nltk.Text(tokens)
-
-def find_similars(word):
-    return analyzed_text.similar(word)
